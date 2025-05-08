9e5c85a450d66d7053054b8035992e9b1b1be301
Vojtech Nerad <vojtech.nerad2@gmail.com>
2025-05-08 20:44:42 +0200

    Add new profile to password store POC

--- src/components/Profiles.tsx
+++ src/components/Profiles.tsx
@@ -1,16 +1,43 @@
 import { Box, Separator, Stack, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import NewProfilePopup from "./NewProfilePopup";
+import { usePasswordStore } from "@/stores/passwordsStore";
 
 export default function Profiles() {
+  const [profileList, setProfileList] = useState<unknown[]>([]);
   const [selectedGroup, setSelectedGroup] = useState<string | null>(null);
+  const [isProfilePopupOpened, setIsProfilePopupOpened] =
+    useState<boolean>(false);
+
+  const groups = usePasswordStore((s) => s.groups);
+  const { addGroup } = usePasswordStore();
+
+  const handleAddProfile = () => {
+    addGroup("aaaaaaaaaaaa");
+  };
 
   return (
-    <Stack padding={4}>
-      <Button>Vše</Button>
-      <Separator />
-      <Box>TODO Profily</Box>
-      <Separator />
-      <Button>Nový profil</Button>
-    </Stack>
+    <>
+      <Stack padding={4} align="stretch">
+        <Button>Vše</Button>
+        <Separator />
+        <Box maxH="100%" overflow="auto" width="100%">
+          <Stack>
+            {groups.map((group) => {
+              return <Button>{group.name}</Button>;
+            })}
+          </Stack>
+        </Box>
+        <Separator />
+        <Button
+          onClick={(prev) => {
+            handleAddProfile();
+          }}
+        >
+          Nový profil
+        </Button>
+      </Stack>
+      <NewProfilePopup isOpened={isProfilePopupOpened} />
+    </>
   );
 }
