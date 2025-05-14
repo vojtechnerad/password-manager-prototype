4414f894a33b421e42e876873e6dd22d77cbdc9a
Vojtech Nerad <vojtech.nerad2@gmail.com>
2025-05-14 04:59:45 +0200

    Light theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Provider>
+    <Provider defaultTheme="light">
       <Dashboard />
       <Toaster />
     </Provider>
